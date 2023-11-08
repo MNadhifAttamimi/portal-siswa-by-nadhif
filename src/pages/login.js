6caf7f57ec9c36cfa28d0022665dabfa9b26ca05
Muhammad Nadhif <nadhif.ppqita@gmail.com>
2023-11-08 13:50:45 +0700

    5

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router'; // Impor useRouter dari next/router
 import styles from '../styles/Login.module.css';
 
-export default function Login({ nextPage, previousPage }) {
-    const login = () => {
-        // Tambahkan logika login di sini
+export default function Login() {
+    const router = useRouter(); // Menggunakan useRouter untuk navigasi
+
+    const [nis, setNis] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginError, setLoginError] = useState('');
+
+    const login = async () => {
+        try {
+            // Lakukan permintaan ke server untuk validasi login
+            const response = await fetch('/api/login', {
+                method: 'POST',
+                body: JSON.stringify({ nis, password }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                // Jika respons dari server menunjukkan login berhasil
+                // Misalnya, server dapat mengirim respons dengan status 200 dan data login berhasil
+                const data = await response.json();
+                // Menggunakan router untuk mengarahkan navigasi ke halaman dashboard
+                router.push('/dashboard');
+            } else {
+                // Handle jika login gagal, menampilkan pesan kesalahan dari server
+                const errorData = await response.json();
+                setLoginError(errorData.message);
+            }
+        } catch (error) {
+            console.error('Login error:', error);
+            setLoginError('An error occurred during login. Please try again.');
+        }
     };
 
     return (
@@ -11,32 +42,40 @@ export default function Login({ nextPage, previousPage }) {
             <div className={styles.loginBox}>
                 <h1 className={styles.title}>Sign in</h1>
                 <p className={styles.enter}>Enter your NIS and password to sign in!</p>
-                <button className={styles.googleSignInButton}>
-                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAjVBMVEVHcEz////9/f38/Pz////4+fn9/f38/P3v8PH19vb///////87q1n8wAnrSz5HifXqOijsUUMfpUYvfvQwqVhDhvRnm/bg7ur74eDykIrznpj609DI5c/uZUj/78rG2PyzzPKJyJj5zMn/++37xSf81XhqsFCa0KV1v4f0kCGvtTCCyJyXt/hyofZDj9Yia4J9AAAAC3RSTlMAojhxnKGw/RkrfoV/rhcAAAFQSURBVDiNlVPZYoMgENQYRCIgclRNNKlpm/T+/88rl3jmofPEMuPu7LpE0X+wO8RpGh9222wW5wFxtuYntJUsaATzBSCaFV/SBhMryF9dm46xrjn5cMzh8p9KRsuypJSVTgLn/o6stKAa7Gqv9r6/kadMQ/NHX8R1u7f5Dc+aGsK6CbxPYY+dLk9rd12PnYQWz6SjbEJMW03M4Y2Qj2ZjFslg4ZUQcrZXTwMug4nUHDRPXqxAFBpKKfFsovSBwGiCYFFCGKiiEKHEYPLza/T2owpRDSaRa/MmeTXwla4j7AmFQd0kxtIrYKEK9R0G5Uy0HGPM+wrC6i41L+A4avez7kYhuYbO9ess+p8VARv0RuEgue0xB/OFaaX5GEvJsTMTFsYX0ZJ3XQP3rQ8nu7+5tLO1ztZrv3w7YM6DaIUMhCwQbDw9+z4SkKYgQdvsA/wBrYgleUhdXncAAAAASUVORK5CYII=" alt="Google Logo" />
-                    <p style={{ marginTop: "4px" }} >Sign in with Google</p>
-                </button>
-
-
-                <p className={styles.or}>or</p>
                 <form id="login-form" className={styles.form}>
                     <div className={styles.inputContainer}>
-                        <label htmlFor="email">NIS</label>
-                        <input type="email" id="email" className={styles.input} placeholder="Enter your NIS" />
+                        <label htmlFor="nis">NIS</label>
+                        <input
+                            type="text"
+                            id="nis"
+                            className={styles.input}
+                            placeholder="Enter your NIS"
+                            value={nis}
+                            onChange={(e) => setNis(e.target.value)}
+                        />
                     </div>
                     <div className={styles.inputContainer}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" className={styles.input} placeholder="Password" />
+                        <input
+                            type="password"
+                            id="password"
+                            className={styles.input}
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
                     <div className={styles.checkboxFlex}>
-                    <div className={styles.checkboxContainer}>
-                        <input type="checkbox" id="keepLoggedIn" className={styles.checkbox} />
-                        <label htmlFor="keepLoggedIn">Keep me logged in</label>
-                    </div>
-                    <div className={styles.checkboxContainer}>
-                        <a href="#">Forget password?</a>
-                    </div>
+                        <div className={styles.checkboxContainer}>
+                            <input type="checkbox" id="keepLoggedIn" className={styles.checkbox} />
+                            <label htmlFor="keepLoggedIn">Keep me logged in</label>
+                        </div>
+                        <div className={styles.checkboxContainer}>
+                            <a href="#">Forget password?</a>
+                        </div>
                     </div>
                     <button type="button" onClick={login} className={styles.signInButton}>Sign In</button>
+                    {loginError && <p className={styles.errorMessage}>{loginError}</p>}
                 </form>
                 <div className={styles.registerText}>
                     Not registered yet?<a href="#">Create an Account</a>
